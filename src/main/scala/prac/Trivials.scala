589c6c49e98dbe0b7d53ddccda0d188c0546336b
manish <manish.c.ghildiyal@gmail.com>
2018-03-24 09:11:51 +0530

    Improvement

--- src/main/scala/prac/Trivials.scala
+++ src/main/scala/prac/Trivials.scala
@@ -1,39 +1,15 @@
 package prac
 
+class Trivials(s:String){
+  private val x = 0
+  private val y = 2
+}
+
 object Trivials {
-
+  private val y = 1
   def main(args: Array[String]): Unit = {
-    //two kinds of variables
-    //val can't be reassigned once initialized,like final variable in java
-    val xVal =  "Hi Scala"
-    //var can be reassigned,like non-final variable in java
-    var xVar = ""
-    xVar = "Hello Scala"
-
-    //type inference
-    var infVar = 8 //infVar referenced as int type
-
-    //explicitly specifying the type
-    var expVar: String = "Scala"
-
-    //defining function in scala
-    def greater(x: Int,y: Int): Int = {
-      if(x > y) x
-      else y
-    }
-
-    println(greater(3,5))
-
-
-    val alphabets = Array("x","y","z")
-    //imperative style for loop
-    for(i <- 0 to alphabets.length-1){
-      println(alphabets(i))
-    }
-    //funtional style for loop
-    alphabets.foreach(str => println(str))//input is a function
-
-    //page 81:Programming in Scala 2nd
+    var Trivials = new Trivials("Trivials")
+    println(Trivials.x)
+    println(Trivials.y)
   }
-
 }
