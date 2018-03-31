307c9bc3701e3c7891a03e4b1b35829257ea09dc
manish <manish.c.ghildiyal@gmail.com>
2018-04-01 00:02:02 +0530

    Tail Recursion

--- src/main/scala/prac/Trivials.scala
+++ src/main/scala/prac/Trivials.scala
@@ -1,5 +1,7 @@
 package prac
 
+import scala.annotation.tailrec
+
 class Trivials(s:String){
   private val x = 0
   private val y = 2
@@ -11,5 +13,23 @@ object Trivials {
     var Trivials = new Trivials("Trivials")
     println(Trivials.x)
     println(Trivials.y)
+
+    // tail-recursive solution
+    def sum(list: List[Int]): Int = {
+      @tailrec
+      def sumWithAccumulator(list: List[Int], currentSum: Int): Int = {
+        list match {
+          case Nil => {
+            val stackTraceAsArray = Thread.currentThread.getStackTrace
+            stackTraceAsArray.foreach(println)
+            currentSum
+          }
+          case x :: xs => sumWithAccumulator(xs, currentSum + x)
+        }
+      }
+      sumWithAccumulator(list, 0)
+    }
+    println(sum(List(1,2,3,4,5,6)))
+
   }
 }
